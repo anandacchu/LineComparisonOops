01a5b50ee4b95e0e349b88ee46691bf0e9d693de
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-26 11:21:02 +0530

    uc3

--- CompareTwoLines.cs
+++ CompareTwoLines.cs
@@ -6,48 +6,27 @@ using System.Threading.Tasks;
 
 namespace LineComparisonOops
 {
-
     public class CompareTwoLines
-
     {
-        public void CheckTwoLines(int l1, int m1, int l2, int m2)
+        public void LengthOfLine(double x1, double y1, double x2, double y2, double a1, double b1, double a2, double b2)
         {
-            int le1, le2;
-            if (l1 == l2)
-            {
-                int comp = m1.CompareTo(m2);
-                if (comp == 0)
-                {
-                    Console.WriteLine("Both line are equals");
-                }
-                else if (comp >= 0)
-                {
-                    Console.WriteLine("Second line is Smaller than first");
-                }
-                else
-                {
-                    Console.WriteLine("Second line is bigger than First");
-                }
-            }
-            else
-            {
-                le1 = m1 - l1;
-                le2 = m2 - l2;
-                int comp = le1.CompareTo(le2);
-                if (comp == 0)
-                {
-                    Console.WriteLine("Both line are equals");
-                }
-                else if (comp >= 0)
-                {
-                    Console.WriteLine("Second line is Smaller than first");
-                }
-                else
-                {
-                    Console.WriteLine("Second line is bigger than First");
-                }
-            }
-        }
-    }
+            double lengthOfLine;
+            double lengthOfLine1;
 
+            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine);
+
+            lengthOfLine1 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine1);
+
+            //0 = lines are equal
+            //1 = second line is smaller
+            //-1 = first line is smaller
+            int result = lengthOfLine.CompareTo(lengthOfLine1);
+            Console.WriteLine("Lines are: " + result);
+
+        }
+        
     }
+   
+ }

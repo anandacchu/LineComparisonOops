1a9091efc7c5a5596bc76599d2a6ae01305443f3
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-26 11:04:59 +0530

    nonstatic

--- CartesianSystemNonStatic.cs
+++ CartesianSystemNonStatic.cs
@@ -8,11 +8,13 @@ namespace LineComparisonOops
 {
     public class CartesianSystemNonStatic
     {
-        public void calculateLength(int x1, int y1, int x2, int y2)
+        public void LengthOfLine(double x1, double y1, double x2, double y2)
         {
-            double sum = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
-            double lineLength = Math.Sqrt(sum);
-            Console.WriteLine("The length of line is:" + lineLength);
+            double lengthOfLine;
+
+
+            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine);
         }
     }
 }
